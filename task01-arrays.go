c139d5f0725a2582f2518be52c26633de7be4d6a
zura <zurabanasashvili@gmail.com>
2022-05-11 11:42:27 +0400

    resolved float32 problem in problem1

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,11 +1,9 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	rs := 0
-	rs = float32(rs)
+	var rs float32 = 0
 	for _, v := range input {
 		rs += v
 	}
-	return rs / float(len(input))
+	return rs / float32(len(input))
 }
