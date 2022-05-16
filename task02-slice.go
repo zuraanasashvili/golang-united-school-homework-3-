2e3a657c7f3e9aeb7c779450df6170aead6d1d7c
zura <zurabanasashvili@gmail.com>
2022-05-16 16:49:48 +0400

    solution for the second exercise

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	var tmp_slice []int64
+	for i := len(input) - 1; i >= 0; i-- {
+		tmp_slice = append(tmp_slice, input[i])
+	}
+	return tmp_slice
 }
